1b3dc2937d608c35e297286d7270c1e4c5d3cae1
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-16 12:54:23 +0300

    feat(storage): add provider model

--- storage/models.py
+++ storage/models.py
@@ -13,3 +13,11 @@ class HtmlStorage(models.Model):
     )
     source_html = models.TextField()
     processed = models.BooleanField(default=False)
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    url = models.URLField(max_length=255, unique=True)
+
+    class Meta:
+        db_table = 'providers'
