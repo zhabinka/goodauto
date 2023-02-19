c8b579dd4f2a1a5afefd24744f3c9115ee2ae289
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-19 16:21:39 +0300

    feat(urlstorage): add type url choice gstfield

--- storage/models.py
+++ storage/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class UrlStorage(models.Model):
+
+    class Type(models.IntegerChoices):
+        SINGLE = 1, 'single'
+        BUNCH = 2, 'bunch'
+
     external_url = models.URLField(max_length=255, unique=True)
     processed = models.BooleanField(default=False)
+    type_url = models.PositiveSmallIntegerField(choices=Type.choices, default=Type.SINGLE)
 
     class Meta:
         db_table = 'url_storage'
