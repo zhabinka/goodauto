bba217bb07217e6a6e6c42daddf6f80615211b7e
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-19 12:59:31 +0300

    feat(storage): remove providers

--- storage/models.py
+++ storage/models.py
@@ -1,22 +1,9 @@
 from django.db import models
 
 
-class Provider(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    url = models.URLField(max_length=255, unique=True)
-
-    class Meta:
-        db_table = 'providers'
-
-
 class UrlStorage(models.Model):
     external_url = models.URLField(max_length=255, unique=True)
     processed = models.BooleanField(default=False)
-    provider = models.ForeignKey(
-        Provider,
-        on_delete=models.CASCADE,
-        default=None,
-    )
 
     class Meta:
         db_table = 'url_storage'
@@ -29,11 +16,6 @@ class HtmlStorage(models.Model):
     )
     source_html = models.TextField()
     processed = models.BooleanField(default=False)
-    provider = models.ForeignKey(
-        Provider,
-        on_delete=models.CASCADE,
-        default=None,
-    )
 
     class Meta:
         db_table = 'html_storage'
@@ -50,10 +32,6 @@ class Brand(models.Model):
 
 class UrlBunchStorage(models.Model):
     node_url = models.URLField(max_length=255)
-    provider = models.ForeignKey(
-        Provider,
-        on_delete=models.CASCADE,
-    )
     brand = models.ForeignKey(
         Brand,
         on_delete=models.CASCADE,
