0fef49d5c97d5d73cd3a61e11d0df7552f6ae2b0
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-19 20:58:38 +0300

    fix(relations): replace type_url field

--- sheduler/models.py
+++ sheduler/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from goodauto.cars.models import Car
-from storage.models import HtmlStorage, UrlStorage
+from storage.models import HtmlStorage, UrlStorage, Type
 
 
 class CrawlerFrontier(models.Model):
@@ -8,6 +8,7 @@ class CrawlerFrontier(models.Model):
         UrlStorage,
         on_delete=models.CASCADE,
     )
+    type_url = models.PositiveSmallIntegerField(choices=Type.choices, default=Type.SINGLE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -20,4 +21,5 @@ class ParserFrontier(models.Model):
         HtmlStorage,
         on_delete=models.CASCADE,
     )
+    type_url = models.PositiveSmallIntegerField(choices=Type.choices, default=Type.SINGLE)
     created_at = models.DateTimeField(auto_now_add=True)
--- storage/models.py
+++ storage/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 
+class Type(models.IntegerChoices):
+    SINGLE = 1, 'single'
+    BUNCH = 2, 'bunch'
+
+
 class UrlStorage(models.Model):
-
-    class Type(models.IntegerChoices):
-        SINGLE = 1, 'single'
-        BUNCH = 2, 'bunch'
-
     external_url = models.URLField(max_length=255, unique=True)
     processed = models.BooleanField(default=False)
-    type = models.PositiveSmallIntegerField(choices=Type.choices, default=Type.SINGLE)
 
     class Meta:
         db_table = 'url_storage'
