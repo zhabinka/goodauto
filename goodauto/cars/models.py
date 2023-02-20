592126ab4f27debc6aa2832d59c092cd084737b6
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-20 11:11:52 +0300

    feat(car): add car_model

--- goodauto/cars/models.py
+++ goodauto/cars/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from storage.models import HtmlStorage, UrlStorage
+from storage.models import CarModel, HtmlStorage, UrlStorage
 
 
 class Car(models.Model):
@@ -14,6 +14,10 @@ class Car(models.Model):
         related_name='html',
     )
     name = models.CharField(max_length=255, blank=True)
+    car_model = models.ForeignKey(
+        CarModel,
+        on_delete=models.CASCADE,
+    )
     price = models.IntegerField(db_index=True, null=True)
     external_id = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
