fc789f277ede1f0fb0a7b09973fa7109577b636a
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-20 11:15:55 +0300

    feat(crawler): add adesa scrapy_bunches

--- sheduler/crawler.py
+++ sheduler/crawler.py
@@ -1,19 +1,22 @@
 import os
 import json
 import time
+import random
+
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import NoSuchElementException
 
 from django.db import transaction
 
 from sheduler.driver import init_chrome_webdriver
 from sheduler.models import CrawlerFrontier
-from storage.models import HtmlStorage, UrlStorage
+from storage.models import HtmlStorage, UrlBunchStorage, UrlStorage, HtmlBunchStorage
 from sheduler.parser import add_parser_task
 from storage import url
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 ASSETS_CAR_LINKS_PATH = os.path.join(BASE_PATH, 'links.json')
-
 
 def add_crawl_task(url_):
     item = url.to_storage(url_)
@@ -73,3 +76,51 @@ def scrapy():
 
     print(f'[INFO] All tasks complited')
 
+
+def scrapy_bunches():
+    for bunch in UrlBunchStorage.objects.filter(processed=False):
+        scrapy_bunch(bunch)
+        print(f'[SUCCESS] {bunch.node_url}')
+
+
+def scrapy_bunch(bunch):
+    driver = init_chrome_webdriver()
+    html = []
+
+    try:
+        driver.get(url=bunch.node_url)
+
+        while True:
+            time.sleep(random.randint(8, 10))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            useful_html = soup.find('div', class_='rc-CarsResultList')
+            html.append(str(useful_html))
+
+            next = driver.find_elements(By.XPATH, "//li[@class='next']/a")
+
+            if len(next):
+                time.sleep(random.randint(3, 5))
+                next[0].click()
+            else:
+                html_bunch, _ = HtmlBunchStorage.objects.get_or_create(
+                    url_bunch_storage=bunch
+                )
+                html_bunch.source_html = ''.join(html)
+                bunch.processed = True
+
+                with transaction.atomic():
+                    bunch.save()
+                    html_bunch.save()
+
+                return
+
+    except Exception as e:
+        # TODO: add logging
+        # TODO: retry requests
+        print(f'[ERROR] {e}')
+    finally:
+        driver.close()
+        driver.quit()
+        print(f'[SUCCESS] All bunches downloaded')
