635186a7d60b2de381d3498b50823542c8a43434
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-20 15:54:24 +0300

    feat(UrlStorage): add car_model field

--- storage/models.py
+++ storage/models.py
@@ -6,9 +6,22 @@ class Type(models.IntegerChoices):
     BUNCH = 2, 'bunch'
 
 
+class CarModel(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+
+    class Meta():
+        db_table = 'car_models'
+        unique_together = ['brand', 'model']
+
+
 class UrlStorage(models.Model):
     external_url = models.URLField(max_length=255, unique=True)
     processed = models.BooleanField(default=False)
+    car_model = models.ForeignKey(
+        CarModel,
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         db_table = 'url_storage'
@@ -24,15 +37,6 @@ class HtmlStorage(models.Model):
 
     class Meta:
         db_table = 'html_storage'
-
-
-class CarModel(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-
-    class Meta():
-        db_table = 'car_models'
-        unique_together = ['brand', 'model']
 
 
 class UrlBunchStorage(models.Model):
