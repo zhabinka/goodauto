8f387ccc8bd7dbea5545dee1bd2366ce8279a7dc
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-21 15:12:00 +0300

    fix(migration): correct doors field default value

--- goodauto/cars/migrations/0006_car_accessories_car_additional_options_car_body_type_and_more.py
+++ goodauto/cars/migrations/0006_car_accessories_car_additional_options_car_body_type_and_more.py
@@ -52,7 +52,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="car",
             name="doors",
-            field=models.PositiveSmallIntegerField(default=""),
+            field=models.PositiveSmallIntegerField(default=0),
             preserve_default=False,
         ),
         migrations.AddField(
