0a399c1b032fe2e6fdf6c8d0089a4830fe8c062a
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-21 20:10:50 +0300

    feat(shell): add ipython autoreload

--- goodauto/settings.py
+++ goodauto/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+IPYTHON_ARGUMENTS = [
+    "-c=%load_ext autoreload\n%autoreload 2",
+    "-i",
+]
