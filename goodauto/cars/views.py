93b05387250974dcc3da6d6f5930fc1e2529479d
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-02-22 16:59:10 +0300

    feat(car): add remove closed car urls function

--- goodauto/cars/views.py
+++ goodauto/cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # from django.contrib.auth import get_user_model
 # from django.views.generic import ListView
 
-from goodauto.cars.models import Car
+from goodauto.cars.models import Car, UrlStorage
 
 
 def index(request):
@@ -18,3 +18,15 @@ def to_storage(html_storage):
         car_model=html_storage.url_storage.car_model
     )
     return item
+
+
+def remove_closed_car_urls():
+    removed_car_ids = []
+    for car in Car.objects.filter(closed=True):
+        url = UrlStorage.objects.get(id=car.url_storage.id)
+        model = '{} {}'.format(car.car_model.brand, car.car_model.model)
+        id = car.id
+        url.delete()
+        removed_car_ids.append((model, id))
+        print(f'[SUCCES] Car {model} ({id}) has removed')
+    return removed_car_ids
