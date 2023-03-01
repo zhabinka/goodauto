84d652d8edad8cdc7c9e63aaf571e72260db5860
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-03-01 20:44:11 +0300

    fix(driver): correct language

--- sheduler/driver.py
+++ sheduler/driver.py
@@ -31,12 +31,12 @@ def init_chrome_webdriver():
 
     # use background mode
     options.add_argument('--start-maximized')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
 
     driver = webdriver.Chrome(service=CHROME_SERVICE, options=options)
     stealth(driver,
-        languages=["en-GB", "en_US", "ru"],
+        languages=["en-GB", "en-US", "ru"],
         vendor="Google Inc. (Intel Inc.)",
         platform="MacIntelWin32",
         webgl_vendor="Intel Inc.",
