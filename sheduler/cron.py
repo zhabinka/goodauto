eb06f165510b262223f756a91baee4ac4ed40fa1
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-03-08 19:01:21 +0300

    feat(cron): add crawler and parser tasks

--- sheduler/cron.py
+++ sheduler/cron.py
@@ -2,8 +2,8 @@ import datetime
 import os
 
 from django_cron import CronJobBase, Schedule
-from sheduler.crawler import scrapy_bunches
-from sheduler.parser import parse_bunches
+from sheduler.crawler import scrapy_bunches, add_crawler_tasks, scrapy
+from sheduler.parser import parse_bunches, add_parser_tasks, parse
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 CRAWLER_LOG_FILE = os.path.join(CUR_DIR, '../logs/cron/crawler.txt')
@@ -11,25 +11,63 @@ PARSER_LOG_FILE = os.path.join(CUR_DIR, '../logs/cron/parser.txt')
 
 
 class RunUrlBunchCrawler(CronJobBase):
-    RUN_EVERY_MINS = 1
+    RUN_EVERY_MINS = 10
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'cron.RunUrlBunchCrawler'
 
     def do(self):
         limit = 1
         scrapy_bunches(limit=limit)
-        message = f'{datetime.datetime.now()} Downloaded {limit} bunch(es) \n'
+        message = f'[BUNCH] {datetime.datetime.now()} Downloaded {limit} bunch(es) \n'
         with open(CRAWLER_LOG_FILE, "a") as f:
             f.write(message)
 
 
-class RunUrlBunchParser(CronJobBase):
+class RunUrlCrawler(CronJobBase):
     RUN_EVERY_MINS = 1
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'cron.RunUrlCrawler'
+
+    def do(self):
+        limit = 2
+        scrapy(limit=limit)
+        message = f'[URL] {datetime.datetime.now()} Downloaded {limit} url(s) \n'
+        with open(CRAWLER_LOG_FILE, "a") as f:
+            f.write(message)
+
+
+class RunUrlParser(CronJobBase):
+    RUN_EVERY_MINS = 20
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'cron.RunUrlBunchParser'
+
+    def do(self):
+        parse()
+        message = f'{datetime.datetime.now()} All urls processed \n'
+        with open(PARSER_LOG_FILE, "a") as f:
+            f.write(message)
+
+
+class RunUrlBunchParser(CronJobBase):
+    RUN_EVERY_MINS = 30
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'cron.RunUrlBunchParser'
 
     def do(self):
         parse_bunches()
         message = f'{datetime.datetime.now()} All bunches processed \n'
+        with open(PARSER_LOG_FILE, "a") as f:
+            f.write(message)
+
+
+class AddShedulerTasks(CronJobBase):
+    RUN_EVERY_MINS = 5
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'cron.AddShedulerTasks'
+
+    def do(self):
+        add_crawler_tasks()
+        add_parser_tasks()
+        message = f'{datetime.datetime.now()} Add tasks to sheduler \n'
         with open(PARSER_LOG_FILE, "a") as f:
             f.write(message)
